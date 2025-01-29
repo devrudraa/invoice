e7d48e591c2c5c130f09232e7e2e7fa4e37ddbf4
Dev Rudra <75237456+devrudraa@users.noreply.github.com>
2025-01-29 20:50:45 +0530

    Added: 1. Prism, 2. Shadcn, 3. Next-auth
    Created:
    1. Login route

---
+++ src/types/env.d.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Next from "next";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      AUTH_SECRET: string;
+      EMAIL_SERVER_USER: string;
+      EMAIL_SERVER_PASSWORD: string;
+      EMAIL_SERVER_HOST: string;
+      EMAIL_SERVER_PORT: number;
+      EMAIL_FROM: string;
+      DATABASE_URL: string;
+    }
+  }
+}
