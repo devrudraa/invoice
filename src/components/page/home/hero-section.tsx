c7e31c951ab4ff11575d4a99df8c835c02799f2e
Dev Rudra <75237456+devrudraa@users.noreply.github.com>
2025-06-25 15:57:19 +0530

    Changed hero section BG, made header responsive, removed @headlessui/react dep

--- src/components/page/home/hero-section.tsx
+++ src/components/page/home/hero-section.tsx
@@ -1,25 +1,26 @@
-import Image from "next/image";
-import React from "react";
 import { heroDetails } from "@/lib/data";
+import React from "react";
 
 const HeroSection: React.FC = () => {
   return (
     <>
       <section
         id="hero"
-        className="relative flex items-center justify-center pb-0 pt-32 md:pt-40 px-5"
+        className="w-full flex items-center justify-center px-5 min-h-[30rem] py-20 md:py-30"
       >
-        <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
+        <div className="absolute left-0 top-0 bottom-0 z-10 w-full">
           <div className="absolute inset-0 h-full w-full bg-hero-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
         </div>
 
-        <div className="absolute inset-0 -z-20 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#6633ee_100%)]"></div>
+        <div className="absolute h-full w-full bg-slate-950 top-0 right-0">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
+        </div>
 
-        <div className="text-center">
-          <h1 className="text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto">
+        <div className="text-center z-20">
+          <h1 className="text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto text-white">
             {heroDetails.heading}
           </h1>
-          <p className="mt-4 text-foreground max-w-lg mx-auto">
+          <p className="mt-4 text-foreground max-w-lg mx-auto text-white">
             {heroDetails.subheading}
           </p>
           <button className="mt-5 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
@@ -28,16 +29,6 @@ const HeroSection: React.FC = () => {
               Get started
             </span>
           </button>
-          <div className="relative group">
-            <Image
-              src={heroDetails.centerImageSrc}
-              width={950}
-              height={900}
-              alt="app mockup"
-              className="relative mt-12 md:mt-16 mx-auto -z-10 rounded-t-lg md:translate-y-32 group-hover:translate-y-0 transition-all duration-200"
-            />
-            <div className="absolute bottom-0 left-0 w-full h-[50%] bg-gradient-to-t from-[#6633ee66] to-transparent pointer-events-none z-10"></div>
-          </div>
         </div>
       </section>
     </>
--- src/components/section/header.tsx
+++ src/components/section/header.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { Transition } from "@headlessui/react";
-import { Menu, X } from "lucide-react";
+import { motion } from "framer-motion";
+import { Menu } from "lucide-react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { siteDetails } from "@/lib/data";
+import { cn } from "@/lib/utils";
 import { menuItem } from "@/types/type";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Container from "./container";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export const menuItems: menuItem[] = [
   {
@@ -26,38 +36,81 @@ export const menuItems: menuItem[] = [
 ];
 
 const Header: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   const { status } = useSession();
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("down");
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentY = window.scrollY;
+      setScrolled(currentY > 50);
+
+      if (currentY > lastScrollY) {
+        setScrollDirection("up"); // hiding header
+      } else {
+        setScrollDirection("down"); // showing header
+      }
+
+      setLastScrollY(currentY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 480);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className="bg-transparent mx-auto w-full">
+    <motion.header
+      animate={{
+        y: scrollDirection === "up" ? "-100%" : "0%",
+        backgroundColor: scrolled
+          ? "rgba(255, 255, 255, 0.7)"
+          : "rgba(255, 255, 255, 0)",
+        boxShadow: scrolled
+          ? "0 2px 10px rgba(0, 0, 0, 0.05)"
+          : "0 0 0 rgba(0, 0, 0, 0)",
+      }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      className={`sticky top-0 right-0 w-full z-50 backdrop-blur-md transition-shadow`}
+    >
       <Container className="!px-0">
-        <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
+        <nav className="shadow-md md:shadow-none bg-transparent md:bg-transparent mx-auto flex justify-between items-center px-5 py-5">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            {/* <IdCardIcon className="text-foreground min-w-fit w-7 h-7" /> */}
             <Image
               src={"/logo.svg"}
               width={40}
               height={40}
               alt="EzyInvoice logo"
             />
-            <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
+            <span
+              className={cn(
+                `manrope text-xl font-semibold text-foreground cursor-pointer`,
+                scrolled ? "text-black" : "text-white"
+              )}
+            >
               {siteDetails.siteName}
             </span>
           </Link>
-
           {/* Desktop Menu */}
           <ul className="hidden md:flex items-center space-x-6">
             {menuItems.map((item) => (
               <li key={item.text}>
                 <Link
                   href={item.url}
-                  className="text-foreground hover:text-foreground-accent transition-colors"
+                  className={cn(
+                    `text-foreground hover:text-foreground-accent transition-colors text-white`,
+                    scrolled ? "text-black" : "text-white"
+                  )}
                 >
                   {item.text}
                 </Link>
@@ -67,7 +120,7 @@ const Header: React.FC = () => {
               {status != "authenticated" ? (
                 <Link
                   href="/login"
-                  className="text-white inline-flex h-10 animate-background-shine items-center justify-center border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-8 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50 rounded-full"
+                  className="text-black inline-flex h-10 items-center justify-center border border-gray-300 bg-white px-8 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50 rounded-full"
                 >
                   Login
                 </Link>
@@ -75,7 +128,7 @@ const Header: React.FC = () => {
                 <Link
                   href="/dashboard"
                   className={
-                    "text-white bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors"
+                    "text-secondary-foreground bg-secondary hover:bg-primary-accent px-5 py-2 rounded-full transition-colors"
                   }
                 >
                   Dashboard
@@ -83,64 +136,76 @@ const Header: React.FC = () => {
               )}
             </li>
           </ul>
-
           {/* Mobile Menu Button */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={toggleMenu}
-              type="button"
-              className="bg-primary text-black focus:outline-none rounded-full w-10 h-10 flex items-center justify-center"
-              aria-controls="mobile-menu"
-              aria-expanded={isOpen}
-            >
-              {isOpen ? (
-                <X className="h-6 w-6" aria-hidden="true" />
-              ) : (
-                <Menu className="h-6 w-6" aria-hidden="true" />
-              )}
-              <span className="sr-only">Toggle navigation</span>
-            </button>
-          </div>
+          <MobileHeader
+            scrolled={scrolled}
+            authenticated={status === "authenticated"}
+          />
         </nav>
       </Container>
-
-      {/* Mobile Menu with Transition */}
-      <Transition
-        show={isOpen}
-        enter="transition ease-out duration-200 transform"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="transition ease-in duration-75 transform"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        <div id="mobile-menu" className="md:hidden bg-white shadow-lg">
-          <ul className="flex flex-col space-y-4 pt-1 pb-6 px-6">
-            {menuItems.map((item) => (
-              <li key={item.text}>
-                <Link
-                  href={item.url}
-                  className="text-foreground hover:text-primary block"
-                  onClick={toggleMenu}
-                >
-                  {item.text}
-                </Link>
-              </li>
-            ))}
-            <li>
-              <Link
-                href="#cta"
-                className="text-black bg-primary hover:bg-primary-accent px-5 py-2 rounded-full block w-fit"
-                onClick={toggleMenu}
-              >
-                Get Started
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </Transition>
-    </header>
+    </motion.header>
   );
 };
 
 export default Header;
+
+const MobileHeader = ({
+  scrolled,
+  authenticated,
+}: {
+  authenticated: boolean;
+  scrolled: boolean;
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className="visible md:hidden" asChild>
+        <Menu
+          className={cn(`size-8`, scrolled ? "text-black" : "text-white")}
+        />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          {/* Only for accessability */}
+          <SheetTitle className="hidden">Nav Menu Items</SheetTitle>
+          <ul className="text-start gap-10">
+            <SheetDescription className="space-y-5">
+              {menuItems.map((item) => (
+                <li key={item.text}>
+                  <Link
+                    href={item.url}
+                    onClick={() => setOpen(false)}
+                    className="text-foreground hover:text-primary block text-lg font-semibold"
+                  >
+                    {item.text}
+                  </Link>
+                </li>
+              ))}
+              {authenticated ? (
+                <li>
+                  <Link
+                    href={"/Dashboard"}
+                    onClick={() => setOpen(false)}
+                    className="text-foreground hover:text-primary block text-lg font-semibold"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <Link
+                    href={"/login"}
+                    onClick={() => setOpen(false)}
+                    className="text-foreground hover:text-primary block text-lg font-semibold"
+                  >
+                    Login
+                  </Link>
+                </li>
+              )}
+            </SheetDescription>
+          </ul>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
